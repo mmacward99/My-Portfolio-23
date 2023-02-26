22a679c050bc41cc13ffcebbe98efb890b78b10f
Ward <mward@EE012.fios-router.home>
2023-02-26 13:50:48 -0500

    spacing

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $('.carousel').carousel({
     interval: 2000
   })
+
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
